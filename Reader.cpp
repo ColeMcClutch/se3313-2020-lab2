55e4a23f6b4c26c21a3d99fc1d0aca334fee90e7
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2024-03-12 22:44:02 -0400

    Update Reader.cpp

--- Reader.cpp
+++ Reader.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+
+
+struct MyShared{
+	std::atomic<int> threadId;
+	std::atomic<int> reportId;
+	std::atomic<double> elapsedTime;
+};
+
+
+//Function to read from shared memory and report
+void readerFunction (MyShared* sharedMemory){
+	while (true){
+		
+	}
+}
+
 
 int main(void)
 {
