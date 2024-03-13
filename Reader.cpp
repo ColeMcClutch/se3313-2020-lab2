1c0628f99b1429b8ac075e588e7f8631e8886a0d
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2024-03-12 23:07:47 -0400

    Update Reader.cpp

--- Reader.cpp
+++ Reader.cpp
@@ -13,7 +13,13 @@ struct MyShared{
 //Function to read from shared memory and report
 void readerFunction (MyShared* sharedMemory){
 	while (true){
-		
+		//Read Shared memory
+		std::cout << "Thread ID: " <<sharedMemory->threadId << ", Report ID: " 
+				  << sharedMemory->reportId << ", Elapsed Time: " 
+                  << sharedMemory->elapsedTime << " seconds" << std::endl;
+			//Time delay before reading again
+			std::this_thread::sleep_for(std::chrono::Seconds(1));
+
 	}
 }
 
@@ -21,4 +27,17 @@ void readerFunction (MyShared* sharedMemory){
 int main(void)
 {
 	std::cout << "I am a reader" << std::endl;
+
+	MyShared sharedMemory;
+
+	//Start Reader thread
+	std::thread readerThread(readerFunction, &sharedMemory)
+
+	//Keep reader running for 10 seconds
+	std::this_thread::sleep_for(std::chrono::seconds(10));
+
+    // Join reader thread
+    readerThread.join();
+
+    return 0;
 }
